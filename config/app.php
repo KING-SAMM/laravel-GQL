68cfee286a36636de95eeea0eb8fcfc84b73460d
kcsamm <kcsamm11@gmail.com>
2020-06-03 11:42:22 +0100

    Populated database with fake data

--- config/app.php
+++ config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        //Added when 'tinker' command failed to work
+        Laravel\Tinker\TinkerServiceProvider::class, 
+
         /*
          * Package Service Providers...
          */
